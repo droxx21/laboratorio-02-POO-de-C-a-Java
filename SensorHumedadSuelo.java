c9d7f1a517a5c753cb207ceefe213e730d1beb3b
David Alonso <drovid1203@gmail.com>
2026-08-19 08:08:57 -0500

    feat: implementación de la clase SensorHumedadSuelo

---
+++ SensorHumedadSuelo.java
@@ -0,0 +1,39 @@
+package AgroTech;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class SensorHumedadSuelo extends Sensor {
+    
+    private double humedadPct;
+
+    public SensorHumedadSuelo(String id, String ubicacion, boolean activo, double humedadPct) {
+        super(id, ubicacion, activo);
+        this.humedadPct = tomarLectura();
+    }
+
+    @Override
+    public double tomarLectura() {
+        if (!isActivo()) {
+            return 0.0;
+        }
+        this.humedadPct = ThreadLocalRandom.current().nextDouble(0.0, 100.0);
+        return this.humedadPct;
+    }
+
+    @Override
+    public String evaluarEstado() {
+        if (!isActivo())
+            return "Sensor Desactivado";
+        
+        if (humedadPct < 30.0) {
+            return "ALERTA: Suelo muy seco (Necesita riego)";
+        } else if (humedadPct > 80.0) {
+            return "ALERTA: Suelo saturado de agua";
+        }
+        return "NORMAL: Humedad óptima";
+    }
+
+    public double getHumedadPct() {
+        return humedadPct;
+    }
+}
